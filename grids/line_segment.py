cb2de7baa412fcf53d900a423f132c3a96f52022
Brady Kelly <brady@bradykelly.net>
2021-12-22 01:22:39 +0200

    Started Day 3

---
+++ grids/line_segment.py
@@ -0,0 +1,12 @@
+from grids.point import Point
+class LineSegment: 
+
+    def __init__(self, start: Point, end: Point) -> None:
+        self.start = start
+        self.end = end
+
+    def __str__(self) -> str:
+        return "Line from ({},{}) to ({},{})".format(self.start.y, self.start.x, self.end.y, self.end.x)    
+
+    def length(self) -> int:
+        return ((self.start.x - self.end.x) ** 2 + (self.start.y - self.end.y) ** 2) ** 0.5
