0cfa6c2fb4069c27c25a0d638616bc956474b15b
Brady Kelly <brady@bradykelly.net>
2021-12-23 00:20:14 +0200

    line segments from directions

--- day3.py
+++ day3.py
@@ -1,3 +1,4 @@
+import copy
 from input_tool import InputTool
 from grids.point import Point
 from grids.line_segment import LineSegment
@@ -6,28 +7,29 @@ tool = InputTool()
 input_lines = tool.input_lines_for_day(3, 2019)
 
 origin = Point(0, 0)
-next = origin
 
 segments = []
 for line in input_lines:
-
     directions  = tool.clean_split(line, ",")
 
+    # Start each wire at the origin
+    next_start = origin
     for dir in directions:
 
-        this_line = LineSegment(start=next, end=next)
+        # Before I follow any direction, the current segment's end point is the same as its start point
+        this_line = LineSegment(start=next_start, end=next_start)
 
         delta = dir[0]
         length = int(dir[1:])
 
         if delta == "U":
-            this_line.end.y = -length
+            this_line.end.y += -length
         elif delta == "D":
-            this_line.end.y = length
+            this_line.end.y += length
         elif delta == "L":
-            this_line.end.x = -length
+            this_line.end.x += -length
         elif delta == "R":
-            this_line.end.x = length
+            this_line.end.x += length
 
-        segments.append(this_line)
-        next = this_line.end
+        segments.append(copy.deepcopy(this_line))
+        next_start = copy.deepcopy(this_line.end)
--- grids/line_segment.py
+++ grids/line_segment.py
@@ -1,9 +1,10 @@
+import copy
 from grids.point import Point
 class LineSegment: 
 
     def __init__(self, start: Point, end: Point) -> None:
-        self.start = start
-        self.end = end
+        self.start = copy.deepcopy(start)
+        self.end = copy.deepcopy(end)
 
     def __str__(self) -> str:
         return "Line from ({},{}) to ({},{})".format(self.start.y, self.start.x, self.end.y, self.end.x)    
